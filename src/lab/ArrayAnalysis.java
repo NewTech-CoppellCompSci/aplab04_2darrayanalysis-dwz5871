a9b0b2c5d669b941d235fe62366c623ebc6c41aa
dwz5871 <90725960+dwz5871@users.noreply.github.com>
2022-10-06 23:45:30 -0500

    submission

--- src/lab/ArrayAnalysis.java
+++ src/lab/ArrayAnalysis.java
@@ -11,89 +11,152 @@ public class ArrayAnalysis {
 
 	
 	//#1
-	public static int[][] createRandom(int row, int column, int min, int max){
+	public static int[][] createRandom(int row, int column, int min, int max){ //add random numbers to arrays
 		
+		int[][] arr = new int[row][column];
+		for (int i = 0; i < row ;i++) { // rows
+			for (int j = 0; j < column ;j++) { // columns
+				arr[i][j] = (int)(Math.random() * (max-min) + min);
+			}
+		}
 		
-		return null;
+		return arr;
 	}
 	
 	
 	//#2
-	public static void printArray(int[][] arr)  {
-		
-		
-	
+	public static void printArray(int[][] arr)  { //prints the arrays
+		for (int[] row: arr)
+		{
+			for (int item: row)
+			{
+				System.out.print(item + "  ");
+			}
+			System.out.println("");
+		}
 	}
 	
 	
 	//#3
-	public static int sumTotal(int[][] arr) {
-		
-		
-		return 0;
+	public static int sumTotal(int[][] arr) { //adds all the ints in the arrays
+		int total = 0;
+		for (int[] row: arr)
+		{
+			for (int item: row)
+			{
+				total += item;
+			}
+		}
+		return total;
 	}
 	
 	
+	
 	//#4
-	public static double average(int[][] arr) {
-		
-		
-		
-		return 0;
+	public static double average(int[][] arr) { //average all the ints in the arrays
+		double average = 0;
+		int numRows = arr.length;
+		int numCols = arr[0].length;
+		average = sumTotal(arr)/(double)(numRows * numCols);
+		return average;
 	}
 	
 	
 	//#5
-	public static int findMin(int[][] arr) {
-		
-		
-		return 0;
+	public static int findMin(int[][] arr) { //finds the minimun int in array
+		int total = 0;
+		int min = 1000;
+		for (int[] row: arr)
+		{
+			for (int item: row)
+			{
+				if (item < min) {
+					min = item;
+				}
+			}
+		}
+		return min;
 	}
 	
 	
 	//#6
-	public static int findMax(int[][] arr) {
-		
-		
-		return 0;
+	public static int findMax(int[][] arr) { //finds the maximun int in array
+		int total = 0;
+		int max = 0;
+		for (int[] row: arr)
+		{
+			for (int item: row)
+			{
+				if (item > max) {
+					max = item;
+				}
+			}
+		}
+		return max;
 	}
 	
 	
 	//#7
-	public static int[][] diffArrays(int[][] arr1, int[][] arr2){
-		
-		
-		
-		return null;
+	public static int[][] diffArrays(int[][] arr1, int[][] arr2){ //finds diffrents in the 2 arrays of they have the same width and length
+		int numRows1 = arr1.length;
+		int numCols1 = arr1[0].length;
+		int numRows2 = arr2.length;
+		int numCols2 = arr2[0].length;
+		if( numRows1 != numRows2 || numCols1 != numCols2) {
+			return null;
+		} else {
+			int[][] diffArr = new int[numRows1][numCols1];	
+			for (int i = 0; i < numRows1; i++) {
+				for (int j = 0; j < numCols1; j++) {
+					diffArr[i][j] = arr1[i][j] - arr2[i][j];
+				}
+			}
+			return diffArr;			
+		}		
 	}
 	
+	public static int[][] shiftDown1(int[][] arr){ //shifts the arrays once
+		int numRow = arr.length;
+		int numCol = arr[0].length;
+		
+		int[][] shiftArr = new int[numRow][numCol];	
+		for (int i = 0; i < numRow; i++) {
+			for (int j = 0; j < numCol; j++) {
+				int newI = i+1;
+				if( newI == numRow )
+					newI = 0;
+				shiftArr[newI][j] = arr[i][j];
+			}
+		}
+		return shiftArr;
+	}
 	
 	//#8
-	public static int[][] transposeRow(int[][] arr1, int dist){
-		
-		
-		return null;
+	public static int[][] transposeRow(int[][] arr1, int dist){	//repeats the shift command dist amount of times	
+		int[][] shifted = arr1;
+		for( int i=0; i<dist; i++ ) {
+			shifted = shiftDown1(shifted);
+		}
+		return shifted;
 	}
 	
 	
 	//#9
-	public static int sum3D(int[][][] arr) {
-		
-		
-		return 0;
+	public static int sum3D(int[][][] arr) { //adds all the ints in all of the arrays
+		int total = 0;
+		for(int[][] outer : arr ) {
+			total += sumTotal(outer);
+		}
+		return total;
 	}
 	
 	
 	//#10
-	public static double average3D(int[][][] arr) {
-		
-		
-		return 0;
+	public static double average3D(int[][][] arr) { //averages all the ints in all of the arrays
+		int numX = arr.length;
+		int numY = arr[0].length;
+		int numZ = arr[0][0].length;
+		return sum3D(arr) / (numX * numY * numZ);
 	}
-	
-	
-	
-	
-	
 	
 }
--- src/lab/Runner.java
+++ src/lab/Runner.java
@@ -1,5 +1,7 @@
 package lab;
 
+import java.util.Scanner;
+
 public class Runner {
 
 	public static void main(String[] args) {
@@ -8,15 +10,37 @@ public class Runner {
 		
 		int[][] arr1 = analysis.createRandom(6, 3, -2, 10);
 		
+		//#1 //prints array, total, average, min, and max
+		int[][] arr2 = analysis.createRandom(10, 1, -2, 10);
+		ArrayAnalysis.printArray(arr2);
+		System.out.println("\nTotal:\n" + analysis.sumTotal(arr2));
+		System.out.println("\nAverage:\n" + analysis.average(arr2));
+		System.out.println("\nMinimum\n" + analysis.findMin(arr2));
+		System.out.println("\nMaximum\n" + analysis.findMax(arr2));
 		
+		//#2 //prints null or differences and the shifted arrays the amount of time you input
+		System.out.println("\nIf the length and width of the array lists are the same it will print the lists\nbut replace the numbers with a driffrent if they arn't then it will print null:\n" + analysis.diffArrays(arr1, arr2));
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("\nHow many times should the arrays shift?");
+		int dist = inKey.nextInt();
+		System.out.println("\nShifted Array\n");
+		analysis.printArray(analysis.transposeRow(arr2, dist));
 		
-		
-		
-		
-		
-		
-		
-		
+		//#3 //makes a 3D array list and prints it
+		int[][][] arr3D = new int[3][4][5];
+		for (int i = 0; i < arr3D.length; i++) {
+			arr3D[i] = analysis.createRandom(4, 5, -40, 40);
+		}
+		int x = 1;
+		for (int[][] arr : arr3D) {
+			System.out.println("\nLayer #" + x);
+			analysis.printArray(arr);
+			x++;
+		}
+
+		//#4 //prints total and average values of the 3D arrays
+		System.out.println("\nTotal Value\n" + analysis.sum3D(arr3D));
+		System.out.println("\nAverage Value\n" + analysis.average3D(arr3D));
 		
 	}
 	
